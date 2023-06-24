90783055ce06addd38ee2529501f16f9ef146dc7
Jawed Alam <jawedalam5130@gmail.com>
2023-06-24 14:35:33 +0530

    display X when Clicking on square

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,7 @@ import Board from './components/Board';
 function App() {
   return (
     <div className="app">
-      <button
-        onClick={() => {
-          console.log('fello');
-        }}
-      >
-        Click me please
-      </button>
+      <Board />
     </div>
   );
 }
